dfd68f7d829787b98733c1b89802baf886d7ad42
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-05-02 17:56:47 +0800

    目录生成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-(function($) {
+(function ($) {
 
- function init() {
+  function init() {
     /* Sidebar height set */
     $sidebarStyles = $('.sidebar').attr('style') || "";
     $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
@@ -11,16 +11,16 @@
     var $contactList = $('#contact-list');
 
     $scontacts.hide();
-    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
-    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
+    $contactList.mouseenter(function () { $scontacts.fadeIn(); });
+    $contactList.mouseleave(function () { $scontacts.fadeOut(); });
 
     /**
      * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
      */
     function activateTab() {
-      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
+      if (['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
         var hash = decodeURIComponent(window.location.hash);
-        if(hash)
+        if (hash)
           $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
         else
           $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
@@ -32,6 +32,30 @@
 
     // initial activation
     activateTab();
+
+    // 目录生成
+    var ele = $('.article_body h1, .article_body h2, .article_body h3, .article_body h4, .article_body h5, .article_body h6');
+    for (let i = 0; i < ele.length; i++) {
+      ele.eq(i).html(`<a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a>`);
+    }
+    var title = { 'H1': 1, 'H2': 2, 'H3': 3, 'H4': 4, 'H5': 5, 'H6': 6 }
+    function dfs(i, type) {
+      var e = $('<ul></ul>')
+      for (; i < ele.length; i++) {
+        if (title[ele.get(i).tagName] > type) {
+          var res = dfs(i, type + 1);
+          i = res[0];
+          e.append(res[1]);
+        } else if (title[ele.get(i).tagName] == type) {
+          e.append(`<li><a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a></li>`)
+        } else {
+          return [i - 1, e];
+        }
+      }
+      return [i - 1, e];
+    }
+    var toc = dfs(1, 1)[1];
+    $('.sidebar').append(toc);
   };
 
   // run init on document ready
