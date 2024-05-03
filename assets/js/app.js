adf14bc6f2a8c7a60111fa0eec271c9aa05f48da
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-05-03 18:36:13 +0800

    share

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,33 +36,44 @@
 
         // 目录生成
         function createTOC() {
-            var ele = $('.article_body h1, .article_body h2, .article_body h3, .article_body h4, .article_body h5, .article_body h6');
-            for (let i = 0; i < ele.length; i++) {
-                ele.eq(i).html(`<a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a>`);
-            }
-            var title = { 'H1': 1, 'H2': 2, 'H3': 3, 'H4': 4, 'H5': 5, 'H6': 6 }
-            function dfs(i, type) {
-                var e = $('<ul></ul>')
-                for (; i < ele.length; i++) {
-                    if (title[ele.get(i).tagName] > type) {
-                        var res = dfs(i, type + 1);
-                        i = res[0];
-                        e.append(res[1]);
-                    } else if (title[ele.get(i).tagName] == type) {
-                        e.append(`<li><a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a></li>`)
-                    } else {
-                        return [i - 1, e];
-                    }
+            if ($('.article_body').length == 1) {
+                var ele = $('.article_body h1, .article_body h2, .article_body h3, .article_body h4, .article_body h5, .article_body h6');
+                for (let i = 0; i < ele.length; i++) {
+                    ele.eq(i).html(`<a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a>`);
                 }
-                return [i - 1, e];
+                var title = { 'H1': 1, 'H2': 2, 'H3': 3, 'H4': 4, 'H5': 5, 'H6': 6 }
+                function dfs(i, type) {
+                    var e = $('<ul></ul>')
+                    for (; i < ele.length; i++) {
+                        if (title[ele.get(i).tagName] > type) {
+                            var res = dfs(i, type + 1);
+                            i = res[0];
+                            e.append(res[1]);
+                        } else if (title[ele.get(i).tagName] == type) {
+                            e.append(`<li><a href="#${ele.eq(i).prop('id')}">${ele.eq(i).html()}</a></li>`)
+                        } else {
+                            return [i - 1, e];
+                        }
+                    }
+                    return [i - 1, e];
+                }
+                var toc = dfs(0, 1)[1];
+                $('.sidebar').append(`<div class="toc"></div>`);
+                $('.toc').append(toc);
             }
-            var toc = dfs(0, 1)[1];
-            $('.sidebar').append(`<div class="toc"></div>`);
-            $('.toc').append(toc);
         }
-        if ($('.article_body').length == 1) {
-            createTOC();
+        createTOC();
+
+        // 配置分享
+        function initShare() {
+            window.nativeShare = new NativeShare()
+            var shareData = {
+                title: document.title,
+                link: location.href,
+            }
+            window.nativeShare.setShareData(shareData)
         }
+        initShare();
     };
 
     // run init on document ready
