bb862d9b99ddab398c5e627de64a249e9aa9c22b
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-05-05 18:09:28 +0800

    把某些样式写成css了，不然用js耗时间

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,10 @@
 (function ($) {
 
     function init() {
-        /* Sidebar height set */
-        $sidebarStyles = $('.sidebar').attr('style') || "";
-        $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
-        $('.sidebar').attr('style', $sidebarStyles);
-
         /* Secondary contact links */
         var $scontacts = $('#contact-list-secondary');
         var $contactList = $('#contact-list');
 
-        $scontacts.hide();
         $contactList.mouseenter(function () { $scontacts.fadeIn(); });
         $contactList.mouseleave(function () { $scontacts.fadeOut(); });
 
